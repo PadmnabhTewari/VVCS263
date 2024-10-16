8d125982fa5c2da93fa5c5ebb02753bf0b50740c
Padmnabh Tewari <91028496+PadmnabhTewari@users.noreply.github.com>
2024-10-16 14:15:58 +0530

    Update mcm.c

--- Lab6/mcm.c
+++ Lab6/mcm.c
@@ -1,14 +1,46 @@
 #include <stdio.h>
+#include <limits.h>
 #include <stdlib.h>
-#include <string.h>
-int memoizedmatrixchain(int p[],int n)
-{
-    for(int i=1;i<=n;i++)
-    {
 
-
-
-
-
+int LookupChain(int **m, int p[], int i, int j) {
+    if (m[i][j] < INT_MAX) {
+        return m[i][j];
     }
-} 
+    if (i == j) {
+        m[i][j] = 0;
+    } else {
+        for (int k = i; k <= j - 1; k++) {
+            int q = LookupChain(m, p, i, k) + LookupChain(m, p, k + 1, j) + p[i - 1] * p[k] * p[j];
+            if (q < m[i][j]) {
+                m[i][j] = q;
+            }
+        }
+    }
+    return m[i][j];
+}
+int memoizedmatrixchain(int p[], int n) 
+{
+    int **m = (int **)malloc(n * sizeof(int *));
+    for (int i = 0; i < n; i++) {
+        m[i] = (int *)malloc(n * sizeof(int));
+    }
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            m[i][j] = INT_MAX;
+        }
+    }
+    int result = LookupChain(m, p, 1, n - 1);  
+    for (int i = 0; i < n; i++) 
+    {
+        free(m[i]);
+    }
+    free(m);
+    return result;
+}
+int main() 
+{
+    int p[] = {};
+    int n = sizeof(p)/sizeof(p[0]);
+    printf("Minimum number of multiplications is %d\n", memoizedmatrixchain(p, n));
+    return 0;
+}
